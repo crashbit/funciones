e36af3cb9b3ba175b4a248b1dedf7152ca497416
German Santos <germans@unam.mx>
2021-08-24 08:43:13 -0500

    Se creo una funcion que regresa una funcion

--- funciones4.dart
+++ funciones4.dart
@@ -3,5 +3,15 @@ void main() {
     return a + b;
   };
 
+  final resultado = aplicaMultiplicador(7);
+
   print(suma(3, 6));
+
+  print(resultado(8));
+}
+
+Function aplicaMultiplicador(num valor) {
+  return (num miValor) {
+    return miValor * valor;
+  };
 }
